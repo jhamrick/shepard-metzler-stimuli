dcbdbbc598ad9a2ab711813ddcff52484a72078a
Jessica B. Hamrick <jhamrick@berkeley.edu>
2017-02-15 18:23:41 -0800

    Skip existing files and allow args to be passed in

--- render_images.py
+++ render_images.py
@@ -1,7 +1,10 @@
 import subprocess as sp
 import os
+import sys
 
 from glob import glob
+
+extra_args = sys.argv[1:]
 
 if not os.path.exists(os.path.join("stimuli", "image")):
     os.mkdir(os.path.join("stimuli", "image"))
@@ -14,4 +17,9 @@ for stim_id in sorted(os.listdir(os.path.join("stimuli", "scene"))):
     for scene in sorted(glob(os.path.join("stimuli", "scene", stim_id, "*.xml"))):
         filename = "{}.png".format(os.path.splitext(os.path.basename(scene))[0])
         result = os.path.join("stimuli", "image", stim_id, filename)
-        sp.check_call(["mitsuba", "-x", "-o", result, scene])
+        if os.path.exits(filename):
+            print("Already exists: {}".format(filename))
+            continue
+
+        cmd = ["mitsuba", "-x"] + extra_args + ["-o", result, scene]
+        sp.check_call(cmd)
